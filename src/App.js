f42e01fdfc08f7be3e79c5bdc3547a3ec3ffc164
nasirdiu <nasir.joypurhat@gmail.com>
2022-03-25 21:04:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import "./App.css";
-import Header from "./componoents/Header/Header";
-import Product from "./componoents/Product/Product";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  useEffect(() => {
-    AOS.init();
-  }, []);
   return (
     <div className="App">
-      <Header />
-      <Product />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
